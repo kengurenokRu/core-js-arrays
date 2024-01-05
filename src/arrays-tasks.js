8d31e565383a46d28ea31a4b8a8e1b9e150be45a
kengrenokRu <nastassiadunaeva@gmail.com>
2024-01-05 21:32:25 +0300

    feat: add solution getFalsyValuesCount

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -380,8 +380,9 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const newArr = arr.filter(Boolean);
+  return (arr.length - newArr.length);
 }
 
 /**
