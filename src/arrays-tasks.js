6df3bfee82029cf7f53308cbc9a7f20967c73c0d
kengrenokRu <nastassiadunaeva@gmail.com>
2024-01-05 21:30:41 +0300

    feat: add solution generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -347,8 +347,9 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const newArr = Array.from({ length: len }, (v, k) => (1 + k * 2));
+  return newArr;
 }
 
 /**
