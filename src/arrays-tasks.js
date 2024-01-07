0212fa73f6c4a8952b5fcd5c94a0a767e7ff80e3
kengrenokRu <nastassiadunaeva@gmail.com>
2024-01-07 18:49:39 +0300

    feat: add solution getHexRGBValues

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -475,8 +475,12 @@ function getIndicesOfOddNumbers(numbers) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  if (arr.length === 0) return [];
+  const newArr = arr.map(function a(el) {
+    return `#${el.toString(16).padStart(6, '0')}`.toUpperCase();
+  });
+  return newArr;
 }
 
 /**
