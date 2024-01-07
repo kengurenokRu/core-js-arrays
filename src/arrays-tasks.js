97d312efcf9ea2bc5f09b14534db636d03738540
kengrenokRu <nastassiadunaeva@gmail.com>
2024-01-07 20:20:03 +0300

    feat: add solution createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -360,8 +360,12 @@ function calculateBalance(/* arr */) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const n = Math.ceil(arr.length / chunkSize);
+  const newArr = Array.from({ length: n }, (el, i) =>
+    arr.slice(i * chunkSize, i * chunkSize + chunkSize)
+  );
+  return newArr;
 }
 
 /**
