9aa84e1d80b952109bfe49cdaeae49d39d19426d
kengrenokRu <nastassiadunaeva@gmail.com>
2024-01-07 18:55:46 +0300

    feat: add solution getMaxItems

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -497,8 +497,13 @@ function getHexRGBValues(arr) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  if (arr.length === 0) return [];
+  return arr
+    .sort(function c(a, b) {
+      return b - a;
+    })
+    .slice(0, n);
 }
 
 /**
