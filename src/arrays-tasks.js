4f146f8059fe9ef1b01c21d3984a552b16781bcc
kengrenokRu <nastassiadunaeva@gmail.com>
2024-01-10 22:50:52 +0300

    feat: add solution calculateBalance and selectMany

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -327,8 +327,10 @@ function flattenArray(nestedArray) {
  *   selectMany([[1, 2], [3, 4], [5, 6]], (x) => x) =>   [ 1, 2, 3, 4, 5, 6 ]
  *   selectMany(['one','two','three'], (x) => x.split('')) =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.reduce((tempArr, el) => {
+    return tempArr.concat(childrenSelector(el));
+  }, []);
 }
 
 /**
@@ -344,8 +346,9 @@ function selectMany(/* arr, childrenSelector */) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  const sum = arr[0][0] - arr[0][1] + (arr[1][0] - arr[1][1]);
+  return sum;
 }
 
 /**
