1fb42e6d2ed85af137228002f5f0534a1c725ab3
kengrenokRu <nastassiadunaeva@gmail.com>
2024-01-11 22:34:30 +0300

    feat: add optimal solution function selectMany

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -337,9 +337,9 @@ function flattenArray(nestedArray) {
  *   selectMany(['one','two','three'], (x) => x.split('')) =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
 function selectMany(arr, childrenSelector) {
-  return arr.reduce((tempArr, el) => {
-    return tempArr.concat(childrenSelector(el));
-  }, []);
+  return arr.flatMap((el) => {
+    return childrenSelector(el);
+  });
 }
 
 /**
